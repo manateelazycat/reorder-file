8c352bb3f34c83c0c7da61127c92514395dd0a1e
Andy Stewart <lazycat.manatee@gmail.com>
2024-10-25 08:34:27 +0800

    Add reorder lines that not start with number.

--- src/reorderfile.clj
+++ src/reorderfile.clj
@@ -2,20 +2,36 @@
   (:require [cloel :as cloel]
             [clojure.string :as str]))
 
-(defn reorder-lines [text]
-  (let [lines (str/split-lines text)
-        numbered-lines (filter #(re-find #"^\d+\." %) lines)
+(defn reorder-numbered-lines
+  "Reorder lines starting with numbers, keeping original format and renumbering"
+  [lines]
+  (let [numbered-lines (filter #(re-find #"^\d+\." %) lines)
         reordered-lines (map-indexed
-                          (fn [idx line]
-                            (str/replace line #"^\d+\." (str (inc idx) ".")))
-                          numbered-lines)]
-    (reduce
-      (fn [acc [old new]]
-        (str/replace acc old new))
-      text
-      (map vector numbered-lines reordered-lines))))
+                         (fn [idx line]
+                           (str/replace line #"^\d+\." (str (inc idx) ".")))
+                         numbered-lines)]
+    [numbered-lines reordered-lines]))
 
-(defn reorder-buffer [buffer-content]
+(defn reorder-alpha-lines
+  "Sort lines alphabetically"
+  [text]
+  (let [lines (str/split-lines text)
+        sorted-lines (sort-by str/lower-case lines)]
+    (str/join "\n" sorted-lines)))
+
+(defn reorder-lines [text]
+  (if (some #(re-find #"^\d+\." %) (str/split-lines text))
+    ;; For lines starting with numbers, use original logic
+    (let [[original-lines sorted-lines] (reorder-numbered-lines (str/split-lines text))]
+      (reduce
+        (fn [acc [old new]]
+          (str/replace acc old new))
+        text
+        (map vector original-lines sorted-lines)))
+    ;; For non-numbered lines, return directly sorted result
+    (reorder-alpha-lines text)))
+
+(defn ^:export reorder-buffer [buffer-content]
   (future
     (let [reordered-content (reorder-lines buffer-content)]
       (if (= reordered-content buffer-content)
